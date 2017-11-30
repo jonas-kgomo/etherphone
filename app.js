62f42d77d3897f88cc26a8e5b8feae38ce113b47
jonas-kgomo <jonaskgmoo@gmail.com>
2017-12-01 02:45:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-what is going on
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (request, response) => {
+  response.send('Hello Express')
+})
+
+app.listen(port, () => {
+  console.log(`Hello Express listening on port ${port}`)
+})
