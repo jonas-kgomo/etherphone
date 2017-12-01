de4967ca466932eda2e109c155efdbf466639847
jonas-kgomo <jonaskgmoo@gmail.com>
2017-12-01 03:58:56 +0300

    visual name

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 
 const app = express()
+const path    = require("path");
+
 const port = process.env.PORT || 8080
 
-app.get('/', (request, response) => {
-  response.send('Hello Express')
-})
+//app.get('/', (request, response) => {
+//  response.send('Hello Express')})
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/visual.html'));
+});
 
 app.listen(port, () => {
   console.log(`Hello Express listening on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
